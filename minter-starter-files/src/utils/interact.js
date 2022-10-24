15d4925dd8027c62069b7de616160085bdaed4d4
mucahit bayar <mucahit.bayar@mavidev.com>
2022-10-24 21:18:33 +0300

    Add utils

--- minter-starter-files/src/utils/interact.js
+++ minter-starter-files/src/utils/interact.js
@@ -35,3 +35,44 @@ export const connectWallet = async () => {
         };
     }
 };
+
+export const getCurrentWalletConnected = async () => {
+    if(window.ethereum) {
+        try {
+            const addressArray = await window.ethereum.request({
+                method: "eth_accounts",
+            });
+            if(addressArray.length > 0) {
+                return {
+                    address: addressArray[0],
+                    status: "Write a message in the text-field above."
+                };
+            } else {
+                return {
+                    address: "",
+                    status: "Connect to Metamask using the top right button."
+                };
+            }
+        } catch (err) {
+            return {
+                address: "",
+                status: err.message
+            };
+        }
+    } else {
+        return {
+            address: "",
+            status: (
+                <span>
+                    <p>
+                        {" "}
+                        {" "}
+                        <a target={"_blank"} href={`https://metamask.io/download.html`}>
+                            You must install metamask, a virtual Ethereum wallet, in your browser
+                        </a>
+                    </p>
+                </span>
+            )
+        }
+    }
+}
