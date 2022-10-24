8446043e3056f06fc8d96f1076a136dfaffcad8a
mucahit bayar <mucahit.bayar@mavidev.com>
2022-10-24 21:55:56 +0300

    Add new dependency

--- minter-starter-files/src/utils/interact.js
+++ minter-starter-files/src/utils/interact.js
@@ -76,3 +76,11 @@ export const getCurrentWalletConnected = async () => {
         }
     }
 }
+
+require('dotenv').config;
+const alchemyKey = process.env.REACT_APP_ALCHEMY_KEY;
+const { createAlchemyWeb3} = require("@alch/alchemy-web3");
+const web3 = createAlchemyWeb3(alchemyKey);
+
+const contractABI = require("../contract-abi.json");
+const contractAddress = "0x14f544FBDf6B159Ec3A6B69E3872cA0cA9b84d93";
