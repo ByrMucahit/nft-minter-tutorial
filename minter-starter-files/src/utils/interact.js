5005ae179d02fdde51c705763798dba8d16213d5
ByrMucahit <m.mucahitbayar@gmail.com>
2022-10-31 22:37:42 +0300

    Add new contract and add new function that provides to make nft

--- minter-starter-files/src/utils/interact.js
+++ minter-starter-files/src/utils/interact.js
@@ -1,3 +1,5 @@
+import { pinJSONToIPFS } from './pinata';
+
 export const connectWallet = async () => {
     /* 'windows.ethereum' is a global API injected by Metamask and other wallet providers that allows websites to request users */
     if (window.ethereum) {
@@ -77,10 +79,63 @@ export const getCurrentWalletConnected = async () => {
     }
 }
 
-require('dotenv').config;
+//require('dotenv').config;
 const alchemyKey = process.env.REACT_APP_ALCHEMY_KEY;
-const { createAlchemyWeb3} = require("@alch/alchemy-web3");
+const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
 const web3 = createAlchemyWeb3(alchemyKey);
 
 const contractABI = require("../contract-abi.json");
 const contractAddress = "0x14f544FBDf6B159Ec3A6B69E3872cA0cA9b84d93";
+
+export const mintNFT = async (url, name, description) => {
+    if(url.trim() === "" || (name.trim() === "" || name.description.trim() === "")) {
+        return {
+            success: false,
+            status: " ! Please make sure all fields are completed before minting.",
+        }
+    }
+
+    //make metadata
+    const metadata = new Object;
+    metadata.name = name;
+    metadata.image = url;
+    metadata.description = description;
+
+    // make pinata call
+    const pinataResponse = await pinJSONToIPFS(metadata);
+    if(!pinataResponse.success) {
+        return {
+            success: false,
+            status: "Something went wrong while uploading your tokenURI.",
+        }
+    }
+    const tokenURI = pinataResponse.pinataUrl;
+
+    window.contract = await new web3.eth.Contract(contractABI, contractAddress);
+
+
+    //set up your Ethereum transaction
+    const transactionParameters = {
+        to: contractAddress,
+        from: window.ethereum.selectedAddress,
+        'data': window.contract.methods.mintNFT(window.ethereum.selectedAddress, tokenURI).encodeABI()
+    }
+
+    // sign transaction via Metamask
+    try {
+        const txHash = await window.ethereum.request ({
+            method: 'eth_sendTransaction',
+            params: [transactionParameters],
+        });
+        return {
+            success: true,
+            status: "Check out your transaction on Etherscan: https://goerli.etherscan.io/tx/" + txHash
+        }
+    } catch(error) {
+        return {
+            success: false,
+            status: "Somethig went wrong: " + error.message
+        }
+    }
+}
+
